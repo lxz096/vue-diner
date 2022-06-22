9a7c48e5a2209efb8f9d97555e1785bfd203ba8c
lxz096 <1535451212@qq.com>
2022-06-22 15:00:01 +0800

    开始页面编写

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,60 @@
 <template>
   <div class="Footer">
-  <div><router-link to="/home">首页</router-link></div>
-<div><router-link to="/card">购物车</router-link></div>
-<div><router-link to="/message">订单</router-link></div>
-<div><router-link to="/mine">我的</router-link></div>
+     <div class="item">
+      <router-link to="/home" class="nav_item" active-class="nav_color">
+        <van-icon name="wap-home-o" size="30" />
+        首页
+      </router-link>
+    </div>
+    <div class="item">
+      <router-link to="/card" class="nav_item" active-class="nav_color">
+        <van-icon name="cart-o" size="30" />
+        购物车
+      </router-link>
+    </div>
+    <div class="item">
+      <router-link to="/message" class="nav_item" active-class="nav_color">
+        <van-icon name="notes-o" size="30" />
+        订单
+      </router-link>
+    </div>
+    <div class="item">
+      <router-link to="/mine" class="nav_item" active-class="nav_color">
+        <van-icon name="user-circle-o" size="30" />
+        我的
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Footer',
+  name: "Footer",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-.hello {
-  font-size: 20px;
-  div {
-    color: red;
+.Footer {
+  display: flex;
+  font-size: 12px;
+  background-color: #fff;
+  border-top: 0.5px solid #eee;
+  .item {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    .nav_color {
+      color: #ffc400;
+    }
+    .nav_item {
+      display: flex;
+      flex-flow: column;
+      align-items: center;
+    }
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'amfe-flexible';
-import { Button } from 'vant';
+import { Button, Icon } from 'vant';
 import router from './router/index'
 
 const app = createApp(App);
 
-app.use(Button);
+app.use(Button).use(Icon);
 app.use(router)
 app.mount('#app');

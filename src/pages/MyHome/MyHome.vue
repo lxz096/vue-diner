6a1bf20198ff1e03d3ab84a118bdc8aeba71acdd
lxz096 <1535451212@qq.com>
2022-06-25 17:41:52 +0800

    首页数据添加

--- src/pages/MyHome/MyHome.vue
+++ src/pages/MyHome/MyHome.vue
@@ -18,10 +18,19 @@
           </div>
           <div class="classify">
             <div class="big_classify">
-              <div>
-                <svg>
-                  <use></use>
+              <div v-for="(i, index) in big_classify" :key="index">
+                <svg class="icon" aria-hidden="true">
+                  <use :xlink:href="`#${i.icon}`"></use>
                 </svg>
+                {{ i.name }}
+              </div>
+            </div>
+            <div class="small_classify">
+              <div v-for="(i, index) in small_classify" :key="index">
+                <svg class="icon" aria-hidden="true">
+                  <use :xlink:href="`#${i.icon}`"></use>
+                </svg>
+                {{ i.name }}
               </div>
             </div>
           </div>
@@ -146,4 +155,106 @@ export default {
   },
 };
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+/deep/ .van-tabs__wrap {
+  border-radius: 10px;
+}
+.home {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+  font-size: 12px;
+  .content {
+    flex: 1;
+    overflow-y: auto;
+
+    .main {
+      margin-top: -30px;
+      .van-tabs {
+        padding: 0 20px 10px;
+      }
+      .main_bg {
+        background-image: linear-gradient(#fff, #f5f5f5);
+        padding: 10px 20px 0 20px;
+        border-radius: 30px 30px 0 0;
+        position: relative;
+        .classify {
+          padding: 20px 0;
+          .small_classify {
+            display: flex;
+            flex-wrap: wrap;
+            margin-top: 20px;
+            div {
+              display: flex;
+              justify-content: center;
+              flex-flow: column;
+              align-items: center;
+              width: 20%;
+            }
+            .icon {
+              width: 30px;
+              height: 30px;
+              margin: 10px;
+            }
+          }
+          .big_classify {
+            display: flex;
+            div {
+              flex: 1;
+              display: flex;
+              justify-content: center;
+              flex-flow: column;
+              align-items: center;
+              .icon {
+                width: 50px;
+                height: 50px;
+                margin-bottom: 5px;
+              }
+            }
+          }
+        }
+
+        .search {
+          position: relative;
+          .search_text {
+            position: absolute;
+            background-color: #ffc400;
+            right: -6px;
+            top: 1px;
+            width: 50px;
+            height: 32px;
+            border-radius: 16px;
+            text-align: center;
+            line-height: 32px;
+            font-size: 14px;
+          }
+          input {
+            width: 100%;
+            border: 1px solid #ffc400;
+            border-radius: 20px;
+            height: 30px;
+          }
+        }
+      }
+    }
+
+    .header {
+      background-image: linear-gradient(#ffc400, #fff);
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 20px 20px 40px 20px;
+      .text {
+        font-size: 20px;
+        font-weight: 600;
+      }
+      .location {
+        span {
+          margin: 0 5px;
+        }
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
